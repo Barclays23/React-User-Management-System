77edf85cf1b8147feecc560b91f901de874d34fc
Barclays23 <itsmesajeer@gmail.com>
2025-07-26 13:41:28 +0530

    env urls updated

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL  || 'http://localhost:5173',
+  // origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  // origin: process.env.FRONTEND_URL,
+    origin: [
+    'http://localhost:5173',
+    'https://react-user-management-system-tau.vercel.app'
+  ],
   credentials: true,
 }));
 
